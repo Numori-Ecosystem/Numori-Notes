b02ba44dd92c8ba3529509327bec6bf61e8c3590
Erik Bjerke <contact@erikbjerke.com>
2026-03-11 03:28:10 +0000

    Bottom toolbar aligment

--- components/NotesList.vue
+++ components/NotesList.vue
@@ -46,26 +46,26 @@
     <div class="flex items-center justify-between px-3 py-2 border-t border-gray-200 dark:border-gray-800">
       <div class="flex items-center gap-1">
         <button @click="$emit('show-templates')"
-          class="p-2 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-850 rounded-lg transition-colors"
+          class="p-2 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-850 rounded-lg transition-colors leading-none"
           title="Templates">
-          <Icon name="mdi:file-document-outline" class="w-5 h-5" />
+          <Icon name="mdi:file-document-outline" class="w-5 h-5 block" />
         </button>
         <button @click="$emit('show-help')"
-          class="p-2 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-850 rounded-lg transition-colors"
+          class="p-2 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-850 rounded-lg transition-colors leading-none"
           title="Help">
-          <Icon name="mdi:help-circle-outline" class="w-5 h-5" />
+          <Icon name="mdi:help-circle-outline" class="w-5 h-5 block" />
         </button>
         <button @click="$emit('show-locale-settings')"
-          class="p-2 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-850 rounded-lg transition-colors"
+          class="p-2 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-850 rounded-lg transition-colors leading-none"
           title="Settings">
-          <Icon name="mdi:cog-outline" class="w-5 h-5" />
+          <Icon name="mdi:cog-outline" class="w-5 h-5 block" />
         </button>
       </div>
       <div class="flex items-center gap-1">
         <button @click="$emit('show-language')"
-          class="p-2 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-850 rounded-lg transition-colors"
+          class="p-2 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-850 rounded-lg transition-colors leading-none"
           title="Language">
-          <Icon name="mdi:translate" class="w-5 h-5" />
+          <Icon name="mdi:translate" class="w-5 h-5 block" />
         </button>
         <ThemeSwitcher />
       </div>
--- components/ThemeSwitcher.vue
+++ components/ThemeSwitcher.vue
@@ -1,9 +1,9 @@
 <template>
   <button @click="toggleTheme"
-    class="p-2 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg transition-colors"
+    class="p-2 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg transition-colors leading-none"
     :title="isDark ? 'Switch to light mode' : 'Switch to dark mode'">
-    <Icon v-if="isDark" name="mdi:weather-sunny" class="w-5 h-5" />
-    <Icon v-else name="mdi:weather-night" class="w-5 h-5" />
+    <Icon v-if="isDark" name="mdi:weather-sunny" class="w-5 h-5 block" />
+    <Icon v-else name="mdi:weather-night" class="w-5 h-5 block" />
   </button>
 </template>
 
